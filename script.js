f63ba8410181fcb87b79cc50758c59f645092c26
giulianoFrau <shokteo@gmail.com>
2021-06-30 15:53:00 +0200

    implementazioni

---
+++ script.js
@@ -0,0 +1,21 @@
+const privacy = document.querySelector(".linkPrivacy");
+const classeNascosta = document.querySelector(".classeNascosta");
+const boxHeight= document.querySelector(".background");
+
+
+function showHidePrivacy() {
+    if(classeNascosta.classList.contains("hidden")){
+        classeNascosta.classList.remove("hidden");
+        boxHeight.style.height="700px";
+    } else{
+    classeNascosta.classList.add("hidden");
+   ;
+    boxHeight.style.height="600px";
+}
+    }
+
+privacy.addEventListener("click", showHidePrivacy);
+
+
+
+
