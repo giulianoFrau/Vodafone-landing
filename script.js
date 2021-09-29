7189dafcfa36ddedbd3d64611b2c6ec99c45fbbd
giulianoFrau <shokteo@gmail.com>
2021-09-29 15:31:49 +0200

    commit

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const privacy = document.querySelector(".linkPrivacy");
 const classeNascosta = document.querySelector(".classeNascosta");
-const boxHeight= document.querySelector(".background");
-
+const boxHeight = document.querySelector(".background");
 
 function showHidePrivacy() {
-    if(classeNascosta.classList.contains("hidden")){
-        classeNascosta.classList.remove("hidden");
-        boxHeight.style.height="700px";
-    } else{
+  if (classeNascosta.classList.contains("hidden")) {
+    classeNascosta.classList.remove("hidden");
+    boxHeight.style.height = "700px";
+  } else {
     classeNascosta.classList.add("hidden");
-   ;
-    boxHeight.style.height="600px";
+    boxHeight.style.height = "600px";
+  }
 }
-    }
 
 privacy.addEventListener("click", showHidePrivacy);
-
-
-
-
